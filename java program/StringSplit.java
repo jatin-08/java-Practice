85dc2882a01b3b364bb92694d269b3d1eb1d7fb8
jatin-08 <yadavjatin137@gmail.com>
2022-10-17 20:16:02 +0530

    initial commit

---
+++ java program/StringSplit.java
@@ -0,0 +1,16 @@
+//  WAP to input a sentence and print each word of the string along with its length in tabular form.
+
+import java.util.*;
+
+public class StringSplit {
+
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the Sentence : ");
+    String str = sc.nextLine();
+    String s[] = str.split("\\s");
+    for (int i = 0; i < str.length(); i++) {
+      System.out.println(s[i]+"\t"+ s[i].length());
+    }
+  }
+}
