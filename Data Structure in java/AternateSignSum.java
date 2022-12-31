aeecd37e6b37acff8bea929b904044a043139752
jatin-08 <yadavjatin137@gmail.com>
2023-01-01 01:05:13 +0530

    initial commit

---
+++ Data Structure in java/AternateSignSum.java
@@ -0,0 +1,29 @@
+// Given a number num. Find the sum of natural number till num but in aternate signs.
+// input: 5
+// output: 1-2+3-4+5 = 3
+
+import java.util.*;
+
+public class AternateSignSum {
+
+  // Creating a function aternateSignSum -> It gives the sum of natural number upto num but in alternate signs.
+  public static int aternateSignSum(int num) {
+    if (num == 1) {
+      return 1;
+    }
+    if (num % 2 == 0) {
+      return aternateSignSum(num - 1) - num;
+    } else {
+      return aternateSignSum(num - 1) + num;
+    }
+  }
+
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the Number: ");
+    int num = sc.nextInt();
+    System.out.print(
+      "Sum of natural number from 1st to " + num + " : " + aternateSignSum(num)
+    );
+  }
+}
---
+++ Data Structure in java/Kmultiple.java
@@ -0,0 +1,30 @@
+// Given a number num and value k. print k multiples of num.
+// constraints-> k>0
+// input: num = 12 , k = 5
+// output: 12,24,36,48,60
+
+import java.util.*;
+
+public class Kmultiple {
+
+  // Creating a function kthMultiple()-> It gives 1st to kth multiple of the given number.
+  public static void kthMultiple(int num, int k) {
+    if (k == 0) { // base case
+      return;
+    }
+    // Recursive work
+    kthMultiple(num, k - 1);
+    //Self work
+    System.out.print(" " + num * k);
+  }
+
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the Number: ");
+    int num = sc.nextInt();
+    System.out.print("Enter the multiple: ");
+    int k = sc.nextInt();
+    System.out.print("1st to " + k + " multiple of " + num + " : ");
+    kthMultiple(num, k);
+  }
+}
