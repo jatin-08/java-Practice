265ea194e9e47d470ffe538a6dda3e225dd0483b
jatin-08 <yadavjatin137@gmail.com>
2023-01-31 00:15:40 +0530

    initial commit

--- Data Structure in java/LargestSumOfSubArray.java
+++ Data Structure in java/LargestSumOfSubArray.java
@@ -2,10 +2,51 @@
 
 import java.util.*;
 
-public class LargestSumOfSubArray{
+public class LargestSumOfSubArray {
 
-  
+  // Creating a inputArray() -> It takes input from the user
+  public static void inputArray(int[] arr, int size) {
+    Scanner sc = new Scanner(System.in);
+    for (int i = 0; i < size; i++) {
+      arr[i] = sc.nextInt();
+    }
+  }
+
+  // Creating a printArray() -> It print the element of the Array.
+  public static void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+
+  // Creating a findLargestSumOfSubArray() -> It return the largest sum subArray.
+  public static int findLargestSumOfSubArray(int[] arr, int size) {
+    int max = Integer.MIN_VALUE;
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+      sum += arr[i];
+      if (max < sum) {
+        max = sum;
+      }
+      if (max < 0) {
+        sum = 0;
+      }
+    }
+    return max;
+  }
+
   public static void main(String[] args) {
-    
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the size: ");
+    int size = sc.nextInt();
+    System.out.print("Enter the element of the Array: ");
+    int[] arr = new int[size];
+    inputArray(arr, size);
+    System.out.println("Printing the element of the Array: ");
+    printArray(arr, size);
+    System.out.println(
+      "The largest sum of Sub Array: " + findLargestSumOfSubArray(arr, size)
+    );
   }
 }
