b4b6d78a24279e909a2622c6cb114385ab4d869a
jatin-08 <yadavjatin137@gmail.com>
2023-03-31 01:54:43 +0530

    initial commit

---
+++ LinkedList/IntroToLinkedList.java
@@ -0,0 +1,23 @@
+package LinkedList;
+
+public class IntroToLinkedList {
+
+  public static class Node {
+
+    int data; // Value
+    Node next; //  address of the next node
+
+    Node(int data) {
+      this.data = data;
+    }
+  }
+
+  public static void main(String[] args) {
+    Node nd1 = new Node(5);
+    Node nd2 = new Node(6);
+    Node nd3 = new Node(1);
+    nd1.next = nd2;
+    nd2.next = nd3;
+       
+  }
+}
