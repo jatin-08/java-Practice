1050ab7c7aaddace26f62355f01470b6a322c914
jatin-08 <yadavjatin137@gmail.com>
2023-04-21 02:50:52 +0530

    done

--- LinkedList/IntroToLinkedList.java
+++ LinkedList/IntroToLinkedList.java
@@ -2,6 +2,17 @@ package LinkedList;
 
 public class IntroToLinkedList {
 
+  // Creating a displayLinkedList() -> It print the element of the list.
+  public static void displayLinkedList(Node head) {
+    System.out.println(
+      "Printing the element of the linked list using While Loop"
+    );
+    while (head != null) {
+      System.out.println(head.data + "  ");
+      head = head.next;
+    }
+  }
+
   public static class Node {
 
     int data; // Value
@@ -18,5 +29,7 @@ public class IntroToLinkedList {
     Node nd3 = new Node(1);
     nd1.next = nd2;
     nd2.next = nd3;
+
+    displayLinkedList(nd1);
   }
 }
